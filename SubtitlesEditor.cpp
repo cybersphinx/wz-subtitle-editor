75b7638c5d4ae7368c68223ecc3874335077c0c6
Michał Dutkiewicz <emdeck@gmail.com>
2011-12-19 19:47:17 +0100

    Add licence headers

--- SubtitlesEditor.cpp
+++ SubtitlesEditor.cpp
@@ -1,3 +1,23 @@
+/***********************************************************************************
+* Warzone 2100 Subtitles Editor.
+* Copyright (C) 2010 - 2011 Michal Dutkiewicz aka Emdek <emdeck@gmail.com>
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*
+***********************************************************************************/
+
 #include "SubtitlesEditor.h"
 #include "ui_SubtitlesEditor.h"
 
@@ -7,8 +27,6 @@
 #include <QtGui/QFileDialog>
 #include <QtGui/QInputDialog>
 #include <QtGui/QDesktopServices>
-
-#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -445,7 +463,7 @@ void MainWindow::showSubtitle()
     else if (currentSubtitle >= subtitles[currentTrack].count())
     {
         currentSubtitle = 0;
-    }    
+    }
 
     if (currentSubtitle < subtitles[currentTrack].count())
     {
